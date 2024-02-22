0e9f9652bbe673b07adc42d9ab4ab0ebcdbe9d6f
Merlinkk <anshulkashyap2504@gmail.com>
2024-02-22 10:27:44 +0530

    server.js edited

---
+++ server.js
@@ -0,0 +1,16 @@
+const Express=require("express");
+const App=Express();
+
+const port=5000;
+
+App.get("/",(req,res)=>{
+    res.send("<h1>h</h1>")
+});
+
+App.listen(port,()=>{
+    console.log(`app is runnig ${port}`)
+})
+
+module.exports = {
+    App
+}
